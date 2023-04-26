2c623849988158a6fa2f43df18d5b964b4f98d04
Jatin Singh <t-jatin3.singh@ocltp.com>
2023-04-26 14:01:31 +0530

    26 April

---
+++ src/components/SearchComponent.jsx
@@ -0,0 +1,72 @@
+import {useState,useEffect} from 'react';
+import {useParams,Link} from 'react-router-dom';
+import {retrieveRootsByIdApi} from './api/rootsApi.js'
+import {useNavigate } from 'react-router-dom';
+import './SearchComponent.scss';
+// import Root from './Root'
+
+export default function SearchComponent(){
+    const [roots,setRoots] = useState([])
+
+    const {values} = useParams()
+
+    const navigate = useNavigate()
+
+    useEffect(
+        () => refreshRoots(values)
+    )
+
+    function refreshRoots(values){
+        retrieveRootsByIdApi(values)
+            .then((response)=> setRoots(response.data) )
+            .catch((error)=> console.log(error))
+    }
+    
+
+    return (
+      <>
+         <div className="SearchComponent container">
+            <h2 className="m-5 text-center"> Your Results appear here</h2>
+            <div className="">
+                <ul className="responsive-table">
+                       <li className="table-header">
+                           
+                                <div class="col col-1"> Lead Id</div>
+                                <div class="col col-2"> Customer Id </div>
+                                <div class="col col-3"> Product Type</div>
+                                <div class="col col-4"> Email </div>
+                            
+                       </li>
+
+                      
+                            {
+                              roots.map(
+                                    root => (
+                                    <li className="table-row" key={root._id}>
+
+                                                <div class="col col-1">
+                                                  <Link to='/root'
+                                                        state = {{root}}
+                                                  >
+                                                    {root._id}
+                                                  </Link>
+                                                </div>
+
+                                                <div class="col col-2"> {root.customerId}</div>
+                                                <div class="col col-3"> {root.productType}  </div>
+                                                <div class="col col-4"> {root.email}  </div>
+                                        </li>
+                                    )
+                                )
+                            }
+                      
+                </ul>
+            </div>
+            <button classNmae = "button1" onClick={() => navigate(-1)}>go back</button>
+        </div>
+
+        
+      </>
+       
+    );
+}
